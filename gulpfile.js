0d6ae7e594cd4a18e3ec558cafd71a7fe2b02ead
Lucky <963190000@qq.com>
2019-01-02 09:26:13 +0800

    上传编译压缩css功能

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,6 @@ var devUglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
 
 
-// 2.以自己的名字作为本地的开发分支，在开发分支进行开发，开发完成后合并到master分支提交到远程的master分支（10分）；
-// 3.使用gulp搭建前端自动化开发环境（10分）；
-// 4.在gulp中使用webserver启动web服务，并且提供自动刷新功能（10分）；
-// 5.在gulp中创建scss任务，进行scss文件编译，并且压缩css（10分）；
 // 6.在gulp中创建js任务编译js文件，合并js，并且压缩（10分）；
 // 7.在gulp中创建watch任务，进行js，css文件监听，自动执行对应的任务（10分）；
 // 8.在gulp中创建default任务，默认执行browserSync服务，js，css，watch任务（10分）；
@@ -25,8 +21,9 @@ var cleanCss = require('gulp-clean-css');
 gulp.task('devSass', function () {
     return gulp.src('./src/scss/*.scss')
         .pipe(devSass()) //编译
-        .pipe(cleanCss()) //压缩
         .pipe(gulp.dest('./src/css/'))
+        .pipe(cleanCss()) //压缩
+        .pipe(gulp.dest('./dist/css/'))
 })
 
 // 启动服务
